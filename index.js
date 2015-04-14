c2250c92985b2543d615c3546bcf8fe4be6b7271
Benjamin Goering <bengoering@gmail.com>
2015-04-14 00:24:52 -0700

    Also listen on an HTTPS port if env configures ssl_{key,cert}

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var log = require('debug')('bengo-web')
-
+var fs = require('fs');
 var server = exports.server = require('./src/server');
 
 if (require.main === module) {
@@ -7,7 +7,25 @@ if (require.main === module) {
 }
 
 function main() {
-  var port = process.env.PORT || 80
-  log('listening on port '+port)
-  server.create().listen(port);
+  var port = process.env.PORT || 80;
+  var httpsPort = process.env.PORT_HTTPS || 443;
+  var httpsCertPath = process.env.ssl_cert;
+  var httpsKeyPath = process.env.ssl_key;
+  var bengoServer = server.create();
+  // HTTP
+  log('listening for HTTP on port '+port)
+  bengoServer.listen(port);
+  // HTTPS (maybe)
+  if (httpsPort && httpsKeyPath && httpsCertPath) {
+    var httpsOptions = {
+      key: fs.readFileSync(httpsKeyPath),
+      cert: fs.readFileSync(httpsCertPath)
+    };
+    log('listening for HTTPS on port '+httpsPort)
+    require('https')
+      .createServer(httpsOptions, bengoServer)
+      .listen(httpsPort)
+  } else {
+    log('skipping HTTPS because no key/cert')
+  }
 }
